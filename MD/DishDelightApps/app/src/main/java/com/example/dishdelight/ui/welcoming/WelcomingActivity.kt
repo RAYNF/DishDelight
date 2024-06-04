171039ff2604d3d3dc783400b3f751b4e17bf51b
RAYNF <rayhannoor33@gmail.com>
2024-06-04 08:30:31 +0700

    update slicing onBoarding
    belum selasi masih progrs onBoading

--- MD/DishDelightApps/app/src/main/java/com/example/dishdelight/ui/welcoming/WelcomingActivity.kt
+++ MD/DishDelightApps/app/src/main/java/com/example/dishdelight/ui/welcoming/WelcomingActivity.kt
@@ -1,4 +1,4 @@
-package com.example.dishdelight.ui.splashscreen
+package com.example.dishdelight.ui.welcoming
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.dishdelight.R
 
-class SplashscreenActivity : AppCompatActivity() {
+class WelcomingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splashscreen)
+        setContentView(R.layout.activity_welcoming)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
