cfa6fe630a41444ac839f6e5363ea8b09d6b2bd9
RAYNF <rayhannoor33@gmail.com>
2024-06-07 00:01:45 +0700

    progres home screen
    progress pengerjaan header homeScreen

--- MD/DishDelightApps/app/src/main/java/com/example/dishdelight/ui/home/HomeFragment.kt
+++ MD/DishDelightApps/app/src/main/java/com/example/dishdelight/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.dishdelight.databinding.FragmentHomeBinding
@@ -28,15 +29,13 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+
         _binding = null
     }
 }
