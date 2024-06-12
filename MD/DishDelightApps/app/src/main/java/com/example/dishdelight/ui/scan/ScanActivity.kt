ad727170d3ed0f194b0e5f46ddf6910b314111e0
RAYNF <rayhannoor33@gmail.com>
2024-06-13 00:06:38 +0700

    FiturCamera dan Galery
    sudah menambahkan fitur camera dan geallery di scan

--- MD/DishDelightApps/app/src/main/java/com/example/dishdelight/ui/scan/ScanActivity.kt
+++ MD/DishDelightApps/app/src/main/java/com/example/dishdelight/ui/scan/ScanActivity.kt
@@ -1,18 +1,26 @@
 package com.example.dishdelight.ui.scan
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.dishdelight.R
 import com.example.dishdelight.databinding.ActivityScanBinding
 import com.example.dishdelight.ui.result.ResultActivity
+import com.example.dishdelight.utils.getImageUri
 
 
-private lateinit var binding: ActivityScanBinding
 class ScanActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScanBinding
+
+    private var currentImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +34,13 @@ class ScanActivity : AppCompatActivity() {
             insets
         }
 
+        binding.btnGaleri.setOnClickListener {
+            startGallery()
+        }
+        binding.btnCamera.setOnClickListener {
+            startCamera()
+        }
+
         binding.analyzeButton.setOnClickListener {
             val intent = Intent(this, ResultActivity::class.java)
             startActivity(intent)
@@ -34,4 +49,43 @@ class ScanActivity : AppCompatActivity() {
 
 
     }
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(this)
+        launcherIntentCamera.launch(currentImageUri!!)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSucces ->
+        if (isSucces) {
+            showImage()
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+
+    }
+
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
+    }
+
+
 }
