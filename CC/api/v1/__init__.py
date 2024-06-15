b07fd08f976153bb87b4e73c5fcf5efa8ef7825e
ekaaryapranata <aryapranata46@gmail.com>
2024-06-15 14:09:02 +0700

    update cc

--- CC/api/v1/__init__.py
+++ CC/api/v1/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
-from .routes import dashboard, add_recipe, favorite_recipe, profiles
+from .routes import dashboard, add_recipe, favorite_recipe, profiles,auth
 
 v1 = Blueprint("v1", __name__)
 v1.register_blueprint(dashboard.dashboard)
 v1.register_blueprint(add_recipe.add_recipe)
 v1.register_blueprint(favorite_recipe.favorite_recipe)
 v1.register_blueprint(profiles.profiles)
+v1.register_blueprint(auth.auth)
