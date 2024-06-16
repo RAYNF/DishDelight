6993fd0b2441c9a03f4456f916ed5e3c8997ffaf
ekaaryapranata <aryapranata46@gmail.com>
2024-06-16 20:05:24 +0700

    update model addrecipe

--- CC/api/v1/models/add_recipe.py
+++ CC/api/v1/models/add_recipe.py
@@ -1,4 +1,22 @@
 from dataclasses import dataclass
 from firebase_admin import auth
-from ..routes.extensions import db
+from ..extensions import db
+
+
+@dataclass
+class add_recipe(db.Model):
+    __tablename__ = "food"
+
+    name_recipes    : str
+    description     : str
+    ingredients     : str
+    
+
+    menu_id: int   = db.Column(db.Integer, primary_key=True)
+    nama_recipes   = db.Column(db.String(50))
+    image_url: str = db.Column(db.String)
+    description    = db.Column(db.String(100))
+    ingredients    = db.Column(db.String(30))
+    instructions   = db.Column(db.String(200))
+    
 
